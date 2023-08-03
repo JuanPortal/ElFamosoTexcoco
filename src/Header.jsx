fb756899f32e0200978a70b92f46a42cfc70ffb4
Portal <juanportalg@gmail.com>
2023-08-02 20:20:25 -0700

    Header updated

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <header>
             <h1>El Famoso Texcoco</h1>
-            <img src="https://firebasestorage.googleapis.com/v0/b/elfamosotexcoco-c1801.appspot.com/o/logo.png?alt=media&token=b12d2b4f-9483-497e-9d1e-4030ff2facbf" title="El Famoso Texcoco logo" alt="El Famoso Texcoco logo" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/elfamosotexcoco-c1801.appspot.com/o/logo.png?alt=media&token=9d657a27-a7b6-4ec4-b23d-cb8bcb9d9518" title="El Famoso Texcoco logo" alt="El Famoso Texcoco logo" />
         </header>
     )
 }
