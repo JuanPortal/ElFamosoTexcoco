61474860a9748c833cd7d95066192ce236f853bb
Portal <juanportalg@gmail.com>
2023-08-02 20:02:44 -0700

    created

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header>
+            <h1>El Famoso Texcoco</h1>
+            <img src="https://firebasestorage.googleapis.com/v0/b/elfamosotexcoco-c1801.appspot.com/o/logo.png?alt=media&token=b12d2b4f-9483-497e-9d1e-4030ff2facbf" title="El Famoso Texcoco logo" alt="El Famoso Texcoco logo" />
+        </header>
+    )
+}
