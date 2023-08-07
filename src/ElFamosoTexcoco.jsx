5b820e154377bb70e62d0b8f128ab1c549e5dc08
Portal <juanportalg@gmail.com>
2023-08-07 15:31:38 -0700

    Menu added

--- src/ElFamosoTexcoco.jsx
+++ src/ElFamosoTexcoco.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from './Header'
+import { Menu } from './Menu'
 
 export const ElFamosoTexcoco = () => {
   return (
     <>
         <Header />
+        <Menu />
     </>
   )
 }
