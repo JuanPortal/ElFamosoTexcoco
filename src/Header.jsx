80f15c8ac696a88a721e17a1760c4e9a06aad313
Portal <juanportalg@gmail.com>
2023-08-07 14:42:53 -0700

    address and number added

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,6 +5,8 @@ export const Header = () => {
         <header>
             <h1>El Famoso Texcoco</h1>
             <img src="https://firebasestorage.googleapis.com/v0/b/elfamosotexcoco-c1801.appspot.com/o/logo.png?alt=media&token=9d657a27-a7b6-4ec4-b23d-cb8bcb9d9518" title="El Famoso Texcoco logo" alt="El Famoso Texcoco logo" />
+            <address>222 W. Slauson Ave. Los Angeles CA 90003</address>
+            <a href="tel:3232351880">(323) 235-1880</a>
         </header>
     )
 }
